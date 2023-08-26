d42b6b4fb8daee879bdd56042e45228e4b874511
kitkat <audiovisual@dambeck.org>
2023-08-26 18:12:19 -0400

    okay this isvery hard. Looking into latex instead

--- render.py
+++ render.py
@@ -1,1 +1,8 @@
-print("makes a zine!")
+from util import html2pdf
+
+file = "http://thelemapedia.org"
+html2pdf.initialize()
+html2pdf.convert(file, "print.pdf")
+input()
+html2pdf.deinitialize()
+exit()
---
+++ util/config.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python3
+
+class Config:
+            DRIVER_TYPE = "firefox"
